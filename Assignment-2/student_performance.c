7ab52228e80073eefec53105fb72f1a186e17e73
Avani Gupta <guptaavani2022@gmail.com>
2025-10-15 20:43:08 +0530

    Fix: Fxed issues with the invalid input for variables and segmented code into functions

--- Assignment-2/student_performance.c
+++ Assignment-2/student_performance.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
 struct Student {
     int rollNo;
@@ -49,54 +50,125 @@ void printDetails(struct Student *students, int *present, int currStudent) {
     printDetails(students, present, currStudent+1);
 }
 
-int main() {
-    int present[101];
-    for(int i = 1; i <= 100; i++) present[i] = 0;
-    //•	Constraints
-    // 1.	Number of students: 1 ≤ N ≤ 100
+void initializePresentArray(int present[]) {
+    for (int i = 1; i <= 100; i++)
+        present[i] = 0;
+}
+
+int getNumberOfStudents() {
     int n = 0;
-    while(n < 1 || n > 100) {
-        printf("Enter total number of students(1<=N<= 100): ");
-        scanf("%d", &n);
-        if(n < 1 || n > 100) printf("Invalid number!!\n");
+    char ch;
+    while (1) {
+        printf("Enter total number of students (1 <= N <= 100): ");
+        if (scanf("%d", &n) != 1) {  
+            while ((ch = getchar()) != '\n' && ch != EOF); 
+            printf("Invalid number!! Please try again.\n");
+            continue;
+        }
+        if (n < 1 || n > 100){
+            printf("Invalid number!! Please try again.\n");
+            continue;
     }
+    break;
+}
+    return n;
+} 
 
-    struct Student students[101];
+void inputName(char name[]) {
+    int valid;
+    char temp[100];
 
-    for (int i = 0; i < n; i++) {
-        printf("Enter Roll number for Student-%d: ", i+1);
-        int rollNumber;
-        scanf("%d",  &rollNumber);
-        if(rollNumber <= 0) {
-            printf("Enter a valid Roll Number(i.e Roll number must be greater than 0)\n");
-            i--;
-            continue;
-        }
-        else if(present[rollNumber] == 1) {
-            printf("Roll Already Exists!\nEnter a unique Roll number!\n");
-            i--;
-            continue;
-        }
-        present[rollNumber] = 1;
-        
-        students[rollNumber].rollNo = rollNumber;
-        printf("Enter Name for Roll number-%d: ", rollNumber);
-        scanf("%s",  &students[rollNumber].name);
-
-
-        //•	Constraints
-        // 1.	Marks in each subject: 0 ≤ Marks ≤ 100
-        for(int j = 0; j < 3; j++) {
-            printf("Enter Marks%d for %s: ", j+1, students[rollNumber].name);
-            scanf("%f", &students[rollNumber].marks[j]);
-            if(students[rollNumber].marks[j] < 0 || students[rollNumber].marks[j] > 100) {
-                printf("Marks must be between 0 and 100\nTry Again\n");
-                j--;
+    while (1) {
+        scanf("%s", temp);  // read input
+        valid = 1;
+        for (int i = 0; temp[i] != '\0'; i++) {
+            if (!isalpha(temp[i])) {  // check if each character is a letter
+                valid = 0;
+                break;
             }
         }
+        if (!valid) {
+            printf("Name should contain only letters. Try again: ");
+        } else {
+            strcpy(name, temp);
+            break;
+        }
+    }
+}
+
+
+int getValidRollNumber(int present[]) {
+    int rollNumber;
+    char ch;
+    while (1) {
+        if (scanf("%d", &rollNumber) != 1) {  // non-numeric input
+            while ((ch = getchar()) != '\n' && ch != EOF); // clear buffer
+            printf("Enter a valid Roll Number(i.e Roll number must be greater than 0)\n");
+            continue;
+        }
+        if (rollNumber <= 0) {
+            printf("Enter a valid Roll Number(i.e Roll number must be greater than 0)\n");
+        } else if (present[rollNumber] == 1) {
+            printf("Roll Already Exists!\nEnter a unique Roll number!\n");
+        } else {
+            present[rollNumber] = 1;
+            break;
+        }
+        printf("Enter Roll number: "); // 
+    }
+    return rollNumber;
+}
+
+void inputMarks(struct Student *student) {
+    float mark;
+    char ch;  // to clear invalid input
+
+    for (int j = 0; j < 3; j++) {
+        while (1) {
+            printf("Enter Marks%d for %s: ", j + 1, student->name);
+            
+            if (scanf("%f", &mark) != 1) {  // non-numeric input
+                while ((ch = getchar()) != '\n' && ch != EOF); // clear buffer
+                printf("Marks must be between 0 and 100\nTry Again\n");
+                continue;
+            }
+
+            if (mark < 0 || mark > 100) {
+                printf("Marks must be between 0 and 100\nTry Again\n");
+            } else {
+                student->marks[j] = mark;
+                break;
+            }
+        }
+    }
+}
+
+void inputStudentData(struct Student students[], int present[], int n) {
+    for (int i = 0; i < n; i++) {
+        printf("Enter Roll number for Student-%d: ", i + 1);
+        int rollNumber = getValidRollNumber(present);
+
+        students[rollNumber].rollNo = rollNumber;
+        printf("Enter Name for Roll number-%d: ", rollNumber);
+        inputName(students[rollNumber].name);
+
+        inputMarks(&students[rollNumber]);
         calculateGrades(&students[rollNumber]);
     }
+}
+
+void processAll(struct Student students[], int present[], int n) {
     printDetails(students, present, 0);
+}
+
+int main() {
+    int present[101];
+    struct Student students[101];
+
+    initializePresentArray(present);
+    int n = getNumberOfStudents();
+    inputStudentData(students, present, n);
+    processAll(students, present, n);
 
     return 0;
 }
